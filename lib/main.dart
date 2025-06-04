6ca4f183161dac4f8d95a4a783d4d067d9ad51ab
xone404 <alfidjokorusmono@gmail.com>
2025-06-04 10:37:54 +0700

    name

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'xone app',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
